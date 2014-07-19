e853cfcdea693331d34e813180ef1b68de68ce66
rageandqq <chitley.sameer@gmail.com>
2014-07-19 01:18:16 -0400

    Added Viewing and Creating functionality, linked with Parse

--- public/js/parseController.js
+++ public/js/parseController.js
@@ -1,26 +1,61 @@
-var app = angular.module('parseApp', []);
-
-app.controller('parseController', ['$scope', function($scope) {
-	Parse.initialize('MQtuM0CbGBUctFLc1L73H1htKmqmKH0CznBbbwDk', 
+Parse.initialize('MQtuM0CbGBUctFLc1L73H1htKmqmKH0CznBbbwDk', 
 		'T70GKfXJnwmtzDECFwXZGBtM67LpnVGvv4U8g4f5');
-	var queryName = 'Sameer';
-	var Game = Parse.Object.extend("Game");
-	var query = new Parse.Query(Game);
-	query.equalTo('name', queryName);
+var Sublet = Parse.Object.extend('Sublet');
+
+var app = angular.module('parseApp', ['ui.bootstrap']);
+
+app.controller('parseCtrl', ['$scope', '$modal', function($scope, $modal) {
+	//Angular model
+	$scope.sublets = [];
+
+	//Parse Initial Query -- read data from Parse App
+	var query = new Parse.Query(Sublet);
 	query.find({
 		success: function(results) {
 			if (results.length > 0) {
-				alert('Object found!');
-			}
-			else {
-				var gameObject = new Game();
-				gameObject.save({name: queryName}).then(function(object) {
-					alert('Object not found. Created new object.');
-				});
+				$scope.sublets = JSON.parse(JSON.stringify(results));
+				$scope.$apply(); //update the $scope -- Parse seems to mess with it.
 			}
 		},
 		error: function(error) {
 			alert('Error: ' + error.code + ' ' + error.message);
 		}
 	});
+
+	//Create a modal, assign template and controller
+	$scope.newSublet = function() {
+		var modalInstance = $modal.open({
+			templateUrl: 'new-sublet.html',
+			controller: 'newSubletCtrl'
+		});
+
+		modalInstance.result.then(function(sublet) { //action on succesful save
+			$scope.sublets.push(sublet); //Do not query again, just append
+		});
+	};
+}]);
+
+app.controller('newSubletCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+	//Angular model
+	$scope.sublet = {};
+
+	$scope.save = function() {
+		//save Angular model as Parse Object
+		var sublet = new Sublet();
+		sublet.set('Address', $scope.sublet.Address);	
+		sublet.set('Price', $scope.sublet.Price);
+		sublet.set('Details', $scope.sublet.Details);
+		sublet.save(null, {
+			success: function(sublet) {
+				$modalInstance.close($scope.sublet); //close on succesful save
+			},
+			error: function(sublet, error) {
+				alert('Failed to create new object, with error code: ' + error.message);
+		  }
+		});
+	};
+
+	$scope.cancel = function() {
+		$modalInstance.dismiss('cancel');
+	};
 }]);
