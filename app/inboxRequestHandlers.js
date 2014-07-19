30775793bdee00289d7073e464eebda549d017e3
influxd <jsacbibit@gmail.com>
2014-07-19 00:07:22 -0400

    Finished up routing and request handler skeletons

---
+++ app/inboxRequestHandlers.js
@@ -0,0 +1,15 @@
+exports.getInbox = function(req, res) {
+
+};
+
+exports.getConversation = function(req, res) {
+
+};
+
+exports.postMessage = function(req, res) {
+
+};
+
+exports.deleteConversation = function(req, res) {
+
+};
